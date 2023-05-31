1aba26c8ffe79f254c3dd6227a98d7558054502d
Vitor <Vitor@DESKTOP-UKJLBM8>
2023-05-31 20:27:46 -0300

    Add project files.

---
+++ GeradorPlanilhaIfogSim2/Form1.cs
@@ -0,0 +1,34 @@
+namespace GeradorPlanilhaIfogSim2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            IniciarVariaveis();
+        }
+
+        #region Variáveis
+
+        #endregion
+
+        #region Propriedades
+
+        #endregion
+
+        #region Métodos
+
+        private void IniciarVariaveis()
+        {
+            cbPlanilhas.Items.Add("Selecione");
+
+        }
+
+        #endregion
+
+        #region Eventos
+
+        #endregion
+    }
+}
